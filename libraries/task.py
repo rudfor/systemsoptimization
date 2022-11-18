aad2b844c157c0217331a5b57ce54500f43fab37
Maria Eleni Kostopoulou <marialena.kostopoulou@outlook.com>
2022-11-19 00:41:06 +0100

    keep assigned event tasks in attribute

--- libraries/task.py
+++ libraries/task.py
@@ -12,7 +12,7 @@ class TaskModel:
     while for ET tasks, where we assume a sporadic model,
     it describes the minimal inter-arrival distance (MIT)
     """
-    def __init__(self, name, computation, period, type, priority, deadline, separation = 0):
+    def __init__(self, name, computation, period, type, priority, deadline, separation = 0, assigned_events = None):
         self.name = name
         self.computation = int(computation) # ci computation
         self.init_computation = int(computation) # Ci initial computation
@@ -22,14 +22,24 @@ class TaskModel:
         self.deadline = int(deadline) # d
         self.init_deadline = int(deadline) # D
         self.separation = int(separation)
+        self.assignedEvents = assigned_events
 
         self.r = 0 # worst-case response time
         self.wcrt = 0
 
+    def __repr__(self):
+        return f"\n Task: " \
+               f"name({self.name}) " \
+               f"computation({self.computation}) " \
+               f"period({self.period}) " \
+               f"type([{self.type})] " \
+               f"deadline({self.deadline}) " \
+               f"separation({self.separation})"
+
 def get_idle():
     return TaskModel('idle', -1, -1, 'Idle', -1, -1)
 
-def get_polling(separation, budget, period, task):
+def get_polling(separation, budget, period, assignedEvents):
     # deadline = period based on 3rd pdf hint
     # priority can be 7 since polling tasks are time triggered tasks
-    return TaskModel(f'tPT{str(separation)}_{task.name}', budget, period, 'PT', 7, period, separation)
+    return TaskModel(f'tPT{str(separation)}', budget, period, 'PT', 7, period, separation, assignedEvents)
